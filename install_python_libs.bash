0409d5eda40c3984c4918817681b5aef280a052b
kushtimusPrime <kushtimusprime@gmail.com>
2024-03-29 07:02:36 -0700

    Bash script for installing Python library

---
+++ install_python_libs.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install torch-scatter==2.1.1
+pip install matplotlib==3.7.2
+pip install matplotlib-inline==0.1.6
