df9024839bfe0b92ecfdca0bad7864f7c09e68ae
kushtimusPrime <kushtimusprime@gmail.com>
2024-03-29 01:10:49 -0700

    Added mamba setup script

---
+++ setup_mamba.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+
+# Set timeout to 1 second between each newline
+set timeout 1
+
+# Spawn the shell script
+spawn bash Mambaforge-Linux-x86_64.sh
+
+# Loop to send newline character 20 times
+for {set i 0} {$i < 170} {incr i} {
+    send "\n"
+    #expect "continue"  # Wait for the next prompt before sending the next newline
+}
+send "yes\n\n"
+# Wait for the script to finish
+expect eof
